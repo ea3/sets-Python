a01ce9b1400d0397d8ec3746864dfad90fbbc682
Emilio Araos <ea3dev@gmail.com>
2020-01-10 13:53:46 -0700

    casting a list to a set

---
+++ sets.py
@@ -0,0 +1,13 @@
+#You can't add repeated items to a set. It won't throw an error, but it won't be added. 
+# sets are unordered collections of unique elements. 
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+myset.add(2)
+print(myset)
+myset.add(2)
+print(myset)
+mylist = [1,1,1,1,1,1,2,2,2,2,3,3,3]
+print(mylist)
+print(set(mylist))
